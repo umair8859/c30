198ba954b37ea8e9693e9cbda6a7db1bb59ecf32
umair8859 <mohsina.sachora@gmai.com>
2020-04-18 13:41:02 +0530

    attach

--- sketch.js
+++ sketch.js
@@ -75,3 +75,21 @@ function mouseDragged(){
 function mouseReleased(){
     slingshot.fly();
 }
+function keyPressed(){
+
+if(keyCode===32){
+
+slingshot.attach(bird.body);
+
+
+
+
+
+}
+
+
+
+
+
+
+}
